05c182e85d5264139b32768cbfc7768c28cfb8ad
19ChArMiL98 <37012397+19ChArMiL98@users.noreply.github.com>
2020-01-04 02:28:19 +0530

    Add files via upload

---
+++ FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //offset the camera behind the player vehicle so that it follows it
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5.0f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float verticalInput;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        //Move the vechicle forward
+        //transform.Translate(0,0,1);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput); //Delta Time represents time interval (1 sec).
+        //Here speed becomes 10m/s
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
